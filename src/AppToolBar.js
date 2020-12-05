8e29d7b5c3ca82dff5996d2d85eb5d8d452c789c
Jacob Solomon <j.solomon@knights.ucf.edu>
2020-12-04 19:10:06 -0500

    feat(toolbar): adds coffee link

--- src/AppToolBar.js
+++ src/AppToolBar.js
@@ -5,31 +5,39 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import FreeBreakfast from '@material-ui/icons/FreeBreakfast';
+import { Tooltip } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      backgroundColor: theme.palette.background.paper,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
+  root: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
 }));
 
-export default function AppToolBar() {
+export default function AppToolBar () {
   const classes = useStyles();
 
   return (
-    <AppBar position="static" classes={{root: classes.root}}>
+    <AppBar position="static" classes={{ root: classes.root }}>
       <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
+        <IconButton disabled edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon/>
         </IconButton>
         <Typography variant="h6" className={classes.title}>
           Phasmophobia Buddy
         </Typography>
+        <Tooltip title="Buy me a coffee!">
+          <IconButton onClick={() => window.open('https://www.buymeacoffee.com/phasmobuddy', '_blank')} edge="start" className={classes.menuButton}
+                      color="inherit" aria-label="menu">
+            <FreeBreakfast/>
+          </IconButton>
+        </Tooltip>
       </Toolbar>
     </AppBar>
   );
